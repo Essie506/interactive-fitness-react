71e665dc37e31468846c9be3c74a66192c7a1ed3
Essie506 <clarke506@gmail.com>
2025-12-13 08:29:09 +0000

    Create Footer.jsx

---
+++ src/Components /Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="text-center p-4 text-[#079992] font-bold">
+      © 2025 Interactive Fitness — All Rights Reserved
+    </footer>
+  );
+}
