0d7f584f1a3142170ee5370e769ba57d6aa675db
Essie506 <clarke506@gmail.com>
2026-06-23 22:28:40 +0100

    Add ComposerTools component and post button

--- src/Components/composer/ComposerTools.tsx
+++ src/Components/composer/ComposerTools.tsx
@@ -1,1 +1,13 @@
 
+   <ComposerTools onMediaUpload={setUploadedMedia} />
+        <button
+          className="btn-post"
+          disabled={!postText.trim() && uploadedMedia.length === 0}
+          onClick={handleSubmit}
+        >
+          Post
+        </button>
+      </div>
+    </>
+  )
+}
