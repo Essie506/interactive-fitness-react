f7ff610e09093807e0a49b85b6f962708b25e4e9
Essie506 <clarke506@gmail.com>
2026-06-23 20:24:15 +0100

    Update and rename src/Components /Footer.jsx to src/Components/menu/SideMenu.tsx

--- src/Components /Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="text-center p-4 text-[#079992] font-bold">
-      © 2025 Interactive Fitness — All Rights Reserved
-    </footer>
-  );
-}
