2951e1b2bd96a0fffdeb638f24dc4927b475fa46
Essie506 <clarke506@gmail.com>
2026-06-26 08:13:23 +0000

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,11 @@
-import { Navbar } from './components/layout/Navbar'
-import { MobileBottomNav } from './components/layout/MobileBottomNav'
 import { MobileTopNav } from './components/composer/mobile/MobileTopNav'
 
 function App() {
   return (
-    <>
-      <div className="mobile-only">
-        <MobileTopNav
-          unreadMessages={3}
-          unreadNotifications={2}
-          onMessagesClick={() => console.log('messages')}
-          onNotificationsClick={() => console.log('notifications')}
-        />
-      </div>
-
-      <div className="desktop-only">
-        <Navbar
-          variant="feed"
-          unreadMessages={2}
-          unreadNotifications={5}
-        />
-      </div>
-
-      <main>
-        Coming Soon...
-      </main>
-
-      <MobileBottomNav />
-    </>
+    <MobileTopNav
+      unreadMessages={3}
+      unreadNotifications={2}
+    />
   )
 }
 
