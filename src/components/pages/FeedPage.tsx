9b1377573d53686cc3f0029e39ee4f34141c65d6
Essie506 <clarke506@gmail.com>
2026-06-28 22:17:23 +0000

    modified:   src/components/pages/FeedPage.tsx

--- src/components/pages/FeedPage.tsx
+++ src/components/pages/FeedPage.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 
 import { useAuth } from '../../context/AuthContext'
 
-import { Navbar } from '../../components/layout/mobile/MobileTopNav'
+import { MobileTopNav } from '../../components/layout/mobile/MobileTopNav'
 import { MobileFeed } from '../../components/layout/MobileFeed'
 import { NavDrawer } from '../../components/layout/NavDrawer'
 import { MobileBottomNav } from '../../components/layout/MobileBottomNav'
