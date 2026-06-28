632217cae5ef7ce99b7821323cebd60f2aef997c
Essie506 <clarke506@gmail.com>
2026-06-28 12:54:15 +0000

    modified:   src/components/router/ProtectedRoute.tsx

--- src/components/router/ProtectedRoute.tsx
+++ src/components/router/ProtectedRoute.tsx
@@ -1,7 +1,7 @@
 // src/router/ProtectedRoute.tsx
 import React, { ReactNode } from 'react'
 import { Navigate } from 'react-router-dom'
-import { useAuth } from '../context/AuthContext'
+import { useAuth } from '../../context/AuthContext'
 
 interface ProtectedRouteProps {
   children:      ReactNode
